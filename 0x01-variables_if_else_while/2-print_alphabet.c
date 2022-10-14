e3771807ebede19703ab9ff84d7164d05611e2fe
solajcodes <olalekansunday638@gmail.com>
2022-10-14 05:32:44 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+		putchar('\n');
+	}
+	return (0);
+}
