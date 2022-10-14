3c003f5e554d10e0c2e0cbc3338f6af02d72b6d0
solajcodes <olalekansunday638@gmail.com>
2022-10-14 06:57:51 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Sucess);
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+}
