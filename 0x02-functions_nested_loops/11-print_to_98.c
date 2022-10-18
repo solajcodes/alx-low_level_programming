fba065d7e0daaf2069fa31a6b510f7a8f50df5bb
solajcodes <olalekansunday638@gmail.com>
2022-10-18 11:49:57 -0700

    c functions and nested loop

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_to_98 - check main
+ *@n: a input integer
+ *function that prints all natural numbers
+ *from n to 98 followed by a new line
+ *Return: nothing
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
