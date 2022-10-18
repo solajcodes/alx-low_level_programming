732e780fb0c6a3e9e887b2a3bf7ccffba7d4d2f7
solajcodes <olalekansunday638@gmail.com>
2022-10-18 09:42:17 -0700

    c functions and nested loop

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 /**
  * _abs - return absolute value of a number
  * @n: parameter
- * Return: absolute value of a number
+ * Return: absolute value
  */
 int _abs(int n)
 {
 	if (n < 0)
 		n = -n;
-		return (n);
+	return (n);
 }
