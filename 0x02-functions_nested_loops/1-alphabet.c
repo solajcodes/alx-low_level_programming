4d84c87d906362bc89323bd97eebf5941672726a
solajcodes <olalekansunday638@gmail.com>
2022-10-18 04:01:26 -0700

    using customized functions to print lower case letters, im very happy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet -> prints the lower case letters
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
