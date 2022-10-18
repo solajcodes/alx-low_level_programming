a008d0ab95ae1b638c34bb421cbc98927d8de8a3
solajcodes <olalekansunday638@gmail.com>
2022-10-18 05:01:08 -0700

    c functions and nested loop

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- * void print_alphabet_x10 -> prints the lower case letters in ten lines
- *
- * Return: always 0
+ * print_alphabet_x10 -> prints the lower case alphabets
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int y;
 	int x;
