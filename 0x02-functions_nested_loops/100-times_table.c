da28e822e42693bbb58bd498dad514f29db84dc8
solajcodes <olalekansunday638@gmail.com>
2022-10-18 12:15:19 -0700

    c functions and nested loop

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,54 @@
+#include "main.h"
+/**
+ * print_times_table - print times table from numbers 0-14
+ * @n: an input integer value
+ * Return: Nothing
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 0 && n < 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
+			_putchar('\n');
+i		}
+	}
+}
+/**
+ * putformat - formatted character to output
+ * @n: number to format
+ * Return: Nothing
+ */
+void putformat(int n)
+{
+	if (n <= 9)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n + '0');
+
+	}
+	else if (n > 9 && n <= 99)
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else
+	{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n / 100 + '0');
+		_putchar(n / 10 % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+}
