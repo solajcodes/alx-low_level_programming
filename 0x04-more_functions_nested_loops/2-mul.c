7eebc6d80216585216733437cc8881180ad744d2
solajcodes <olalekansunday638@gmail.com>
2022-10-20 06:24:21 -0700

    debugging in c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - takes 2 variable and multiply them
+ * @a: first variable
+ * @b: second variable
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
