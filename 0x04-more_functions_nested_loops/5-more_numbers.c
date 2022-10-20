bf7582ea51958c5a3dfb7e7a6284d83ac8f06d36
solajcodes <olalekansunday638@gmail.com>
2022-10-20 08:41:52 -0700

    more functions nested loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			_putchar(b + '0');
+		}
+		_putchar('\n');
+	}
+}
