a3494b059a97bcfc2798976e5bcb8e6237c3d8c1
solajcodes <olalekansunday638@gmail.com>
2022-10-20 08:24:01 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,13 +3,14 @@
 /**
  * print_numbers - prints number from 0-9
  */
+
 void print_numbers(void)
 {
 	int x;
 
 	for (x = 0; x < 10; x++)
 	{
-		_putchar(x);
+		_putchar(x + '0');
 	}
 	_putchar('\n');
 }
