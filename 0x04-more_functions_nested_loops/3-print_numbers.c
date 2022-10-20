9be95ce68623096b63303c4530be63cf31ada9e2
solajcodes <103960554+solajcodes@users.noreply.github.com>
2022-10-20 15:40:02 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * print_numbers - prints number from 0-9
- *Return: Always 0.
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
  */
-
 void print_numbers(void)
 {
 	int ch;
