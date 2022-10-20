3925724021dcd27a6f1eb0d6eed9bc9241cb1c13
solajcodes <olalekansunday638@gmail.com>
2022-10-20 07:56:58 -0700

    more functions nested loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,9 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * print_numbers - prints number from 0-9
-=======
- * print_numbers - prints numbers between 0 to 9.
- * Return: no return.
->>>>>>> 9be95ce68623096b63303c4530be63cf31ada9e2
  */
+
 void print_numbers(void)
 {
 	int x;
