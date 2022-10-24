db5509a17725668fb76766026e044ed8e4f71e05
solajcodes <olalekansunday638@gmail.com>
2022-10-24 12:05:09 -0700

    pointers and arrays strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - print the last half of a string
+ * @s: pointer to the string variable s
+ */
+
+void puts_half(char *s)
+{
+	int len = strlen(s), i;
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; i < len; i++)
+			_putchar(s[i]);
+	}
+	else
+		for (i = (len / 2) + 1; i < len; i++)
+			_putchar(s[i]);
+
+	_putchar('\n');
+}
