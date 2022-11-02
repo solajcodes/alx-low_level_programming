c4cbd748f786f544a34503aceee1aefb4bf8b7f6
solajcodes <103960554+solajcodes@users.noreply.github.com>
2022-11-02 14:28:23 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,34 @@
+
 #include "main.h"
-
 /**
- * int check_prime(int n, int resp);
- * is_prime_number - check if n is prime number
- * @n: integer to be checked
- *
- * Return: i if is prime 0 otherwise
+ * is_prime - detects if an input number is a prime number.
+ * @n: input number.
+ * @c: iterator
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+			return (1);
+		else
+			return (0);
+	}
+	return (0 + is_prime(n, c + 1));
+}
+/**
+ * is_prime_number - detects if an input number is a prime number.
+ * @n: input number.
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
 int is_prime_number(int n)
 {
-	return (check_prime(n, 2));
-}
-
-/**
- * check_prime - check all number < n if they can divide it
- *  @n: int
- *  @resp: int
- *  Return: int
- */
-
-int check_prime(int n, int resp)
-{
-	if (resp >= n && n > 1)
-		return (1);
-	else if (n % resp == 0 || n <= 1)
+	if (n == 0)
 		return (0);
-	else
-		return (check_prime(n, resp + 1));
+	if (n < 0)
+		return (0);
+	if (n == 1)
+		return (0);
+	return (is_prime(n, 2));
 }
