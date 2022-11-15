5d4014fc035f97e7d02362afe47d6ac877a55445
solajcodes <103960554+solajcodes@users.noreply.github.com>
2022-11-15 04:45:03 +0100

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,19 +1,27 @@
-#ifndef DOG_H
-#define DOG_H
+
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog - structure definition of a dog
- * @name: character string
- * @age: age integer
- * @owner: character string
+ * dog_t - Typedef for struct dog
  */
+typedef struct dog dog_t;
 
-typedef struct dog
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
