44d5f16ff882451d910b8d1a6aa1fbf767696330
solajcodes <olalekansunday638@gmail.com>
2022-12-01 09:18:59 -0800

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include<string.h>
-#include<main.h>
-#include<math.h>
+#include <string.h>
+#include "main.h"
+#include <math.h>
 
 /**
- * binary_to_unit - converts to unit
- * @b: Pointer to the character array in binary to be converted
+ * binary_to_uint - converts binary to uint
+ * @b: pointer to the character array in binary to be converted
  *
- * Return: converted number if successful, 0 if not successful
+ * Return: converted number if successful, 0 otherwise
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -15,13 +15,13 @@ unsigned int binary_to_uint(const char *b)
 	int i, base = 1, len = 0;
 	unsigned int res = 0;
 
-	if(!b)
-		return 0;
-	
+	if (!b)
+		return (0);
+
 	len = strlen(b);
 
 	for (i = len - 1; i >= 0; i--)
-		{
+	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		if (b[i] == '1')
