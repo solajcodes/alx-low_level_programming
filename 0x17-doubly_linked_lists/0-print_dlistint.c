8332fad83835127ca5cd9256a9d39e1772c0ea6e
solajcodes <olalekansunday638@gmail.com>
2023-01-13 13:14:12 +0100

    double linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - prints all the elements of a
+ * dlistint_t list
+ *
+ * @h: head of the list
+ * Return: the number of nodes
+ */
+
 size_t print_dlistint(const dlistint_t *h) {
     size_t count = 0;
     const dlistint_t *current = h;
