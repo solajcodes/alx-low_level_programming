7a67721316739112291c2806282136f029b3eced
solajcodes <olalekansunday638@gmail.com>
2023-01-13 13:51:35 +0100

    double linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,13 +8,17 @@
  * Return: the number of nodes
  */
 
-size_t print_dlistint(const dlistint_t *h) {
-    size_t count = 0;
-    const dlistint_t *current = h;
-    while (current != NULL) {
-        printf("%d\n", current->n);
-        current = current->next;
-        count++;
-    }
-    return count;
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	const dlistint_t *current = h;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count++;
+	}
+
+	return (count);
 }
