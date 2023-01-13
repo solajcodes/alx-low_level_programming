6f0026f5e225ec6e3433de30d049b9fda92bc1e7
solajcodes <olalekansunday638@gmail.com>
2023-01-13 15:21:34 +0100

    counting the length of a list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in
+ * a double linked list
+ *
+ * @h: head of the list
+ * Return: the number of elements in the lists
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
