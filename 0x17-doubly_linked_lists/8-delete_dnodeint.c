ba3685e918133f8f7bd735c0e66ba03f61d1afb3
solajcodes <olalekansunday638@gmail.com>
2023-01-13 17:29:27 +0100

    a function that deletes the node at index index of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,44 +8,45 @@
  * @index: index of the new node
  * Return: 1 if it succeeded, -1 if it failed
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *current = *head;
+	dlistint_t *h1;
+	dlistint_t *h2;
 	unsigned int i;
 
-	if (!*head)
-	{
-		return (-1);
-	}
+	h1 = *head;
 
-	if (index == 0)
+	if (h1 != NULL)
+		while (h1->prev != NULL)
+			h1 = h1->prev;
+
+	i = 0;
+
+	while (h1 != NULL)
 	{
-		*head = current->next;
-		if (current->next)
+		if (i == index)
 		{
-			current->next->prev = NULL;
+			if (i == 0)
+			{
+				*head = h1->next;
+				if (*head != NULL)
+					(*head)->prev = NULL;
+			}
+			else
+			{
+				h2->next = h1->next;
+
+				if (h1->next != NULL)
+					h1->next->prev = h2;
+			}
+
+			free(h1);
+			return (1);
 		}
-		free(current);
-		return (1);
+		h2 = h1;
+		h1 = h1->next;
+		i++;
 	}
 
-	for (i = 0; i < index - 1 && current; i++)
-	{
-		current = current->next;
-	}
-
-	if (!current || !current->next)
-	{
-		return (-1);
-	}
-
-	current->next = current->next->next;
-	if (current->next)
-	{
-		current->next->prev = current;
-	}
-	free(current->next);
-
-	return (1);
+	return (-1);
 }
