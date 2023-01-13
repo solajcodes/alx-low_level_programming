15c7f96caea8e3412c1e2eab373c95a7b7302d05
solajcodes <olalekansunday638@gmail.com>
2023-01-13 13:06:07 +0100

    double linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h) {
+    size_t count = 0;
+    const dlistint_t *current = h;
+    while (current != NULL) {
+        printf("%d\n", current->n);
+        current = current->next;
+        count++;
+    }
+    return count;
+}
